cc87ee27798247e414dcebe9309ed247c81d35b0
Ayowalexy <shalom.mathew@wayapaychat.com>
2023-05-16 16:43:52 +0100

    fixes

--- routes/automatic-statement/automatic-statement.js
+++ routes/automatic-statement/automatic-statement.js
@@ -8,8 +8,8 @@ router.route('/analysis')
     .post(protect, automateStatementAnalysis)
     .get(protect, isAuto, getAutomaticStatementAnalysis)
 
-router.route('/analysis/:key')
-    .put(protect, confirmAutomaticCredentials);
+router.route('/analysis-statement/:key')
+    .post(protect, confirmAutomaticCredentials);
 // .get(protect, isAutomatic, getAutomaticProcessingStatus)
 
 
